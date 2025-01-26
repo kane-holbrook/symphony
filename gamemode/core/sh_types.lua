e55aad7aaecade0aa041509eeb9b114dc4bdf39b
Kane Holbrook <holbrook.kane@gmail.com>
2025-01-26 06:12:37 -0800

    Proxies and tests

--- gamemode/core/sh_types.lua
+++ gamemode/core/sh_types.lua
@@ -423,6 +423,28 @@ do
 	PROXY.Prototype.Set = PROXY.Prototype.SetValue
 	PROXY.Prototype.Get = PROXY.Prototype.GetValue
 	
+	function PROXY.Prototype:Initialize()
+		self.Event = Type.New(EVENTBUS)
+	end
+
+	function PROXY.Prototype:Hook(...)
+		return self.Event:Hook("Change", ...)
+	end
+
+	function PROXY.Prototype:Unhook(...)
+		return self.Event:Unhook("Change", ...)
+	end
+
+	function PROXY.Prototype:SetValue(value)
+		local old = self.Value
+		self.Value = value
+		return self.Event:Run("Change", value, old)
+	end
+
+	function PROXY.Metamethods:__tostring()
+		return "Proxy[" .. stringex.ToString(self.Value) .. "]"
+	end
+	
 
 	local PRIMITIVE = Type.Register("Primitive", PROXY)
 	
@@ -598,6 +620,27 @@ hook.Add("Sym:RegisterTests", "sym/sh_types.lua", function ()
 		er = ev:Run("Event", 32)
 		Test.Equals(er:GetCancelled(), true)
 		Test.Equals(r, true)
+	end)
+
+	root:AddTest("Proxy", function ()
+		local proxy = Type.New(Type.Proxy)
+		local valueChanged = false
+
+		local id = proxy:Hook(function(newValue, oldValue)
+			Test.Equals(oldValue, nil)
+			Test.Equals(newValue, 42)
+			valueChanged = true
+		end)
+
+		proxy:SetValue(42)
+		Test.Equals(proxy:GetValue(), 42)
+		Test.Equals(valueChanged, true)
+		Test.Equals(tostring(proxy), "Proxy[42]")
+
+		proxy:Unhook(id)
+		
+		proxy:SetValue("Hello")
+		Test.Equals(tostring(proxy), "Proxy[\"Hello\"]")
 	end)
 end)
 
